266d9ca8376584e0f232d61a089ee595da39f1e8
franckniat <niatmanuel2002@gmail.com>
2024-01-24 21:19:47 +0100

    hide firebase config

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,16 +1,17 @@
 
+import { env } from "@/env";
 import { initializeApp } from "firebase/app";
 import { getAuth} from 'firebase/auth';
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDVg0nuMIM9tHZy4U8_NoAH_tVUxGq2cvo",
-  authDomain: "niatoai.firebaseapp.com",
-  projectId: "niatoai",
-  storageBucket: "niatoai.appspot.com",
-  messagingSenderId: "743785166801",
-  appId: "1:743785166801:web:ffc5062a683d0713c6d2f0"
+  apiKey: env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
